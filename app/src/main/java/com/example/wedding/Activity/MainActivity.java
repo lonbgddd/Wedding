3f30a6d6d0a1364b436da30e939b05ebe80ea287
nobita7801 <nobita7801@gmail.com>
2022-12-02 07:52:37 +0700

    database

--- app/src/main/java/com/example/wedding/Activity/MainActivity.java
+++ app/src/main/java/com/example/wedding/Activity/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.wedding.Activity;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -7,14 +10,9 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.LinearLayout;
-
-import com.example.wedding.Adapter.PopularAdapter;
 import com.example.wedding.Domain.Popular;
+import com.example.wedding.Helper.Sqldatabase;
 import com.example.wedding.R;
-import com.example.wedding.databinding.ActivityMainBinding;
 import com.example.wedding.fragmnet.CartFragment;
 import com.example.wedding.fragmnet.ContactFragment;
 import com.example.wedding.fragmnet.HomeFragment;
@@ -24,6 +22,10 @@ import com.google.android.material.navigation.NavigationBarView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter, adapter2;
+    private RecyclerView recyclerViewPopularlist;
+    private Sqldatabase sqldatabase;
+
     private ActivityMainBinding binding = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +51,29 @@ public class MainActivity extends AppCompatActivity {
                         getSupportFragmentManager().beginTransaction().replace(R.id.fade_control, UserFragment.newInstance()).commit();
                         break;
 
+        sqldatabase = new Sqldatabase(this);
+
+
+        recyclerViewPopular();
+    }
                 }
 
+
+    private void recyclerViewPopular() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerViewPopularlist = findViewById(R.id.view1);
+        recyclerViewPopularlist.setLayoutManager(linearLayoutManager);
+        recyclerViewPopularlist.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewPopularlist.addItemDecoration(new DividerItemDecoration(MainActivity.this, LinearLayoutManager.VERTICAL));
                 return true;
             }
         });
 
+        ArrayList<Popular> popularlist = new ArrayList<>();
+        popularlist.add(new Popular(1, "Tôm hùm", "hi hi hi", R.drawable.mon1));
+        popularlist.add(new Popular(1, "Tôm hùm", "hi hi hi", R.drawable.mon1));
+        popularlist.add(new Popular(1, "Tôm hùm", "hi hi hi", R.drawable.mon1));
+        popularlist.add(new Popular(1, "Tôm hùm", "hi hi hi", R.drawable.mon1));
 
     }
 
