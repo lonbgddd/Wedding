c9aba8cee9b664bd99e7302a9ad08a61824286be
nobita7801 <nobita7801@gmail.com>
2022-12-04 21:08:44 +0700

    truyen data

--- app/src/main/java/com/example/wedding/Helper/Sqldatabase.java
+++ app/src/main/java/com/example/wedding/Helper/Sqldatabase.java
@@ -27,21 +27,21 @@ public class Sqldatabase extends SQLiteOpenHelper {
                 "Id_monan INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "TenMon TEXT NOT NULL," +
                 "Anh TEXT NOT NULL," +
-                "Gia INTEGER NOT NULL)";
-
+                "DanhGia TEXT NOT NULL)";
         db.execSQL(createTableMonan);
 
         String createTableKhonggiantiec = "create table Khonggiantiec(" +
                 "Id_khonggiantiec INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "Tenkhonggian TEXT NOT NULL," +
+                "DanhGia TEXT NOT NULL," +
                 "Anh TEXT NOT NULL)";
-
         db.execSQL(createTableKhonggiantiec);
 
         String createTableDichvu = "create table Dichvu(" +
                 "Id_dichvu INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "Khonggiantiec INTEGER NOT NULL," +
                 "Ten TEXT NOT NULL," +
+                "DanhGia TEXT NOT NULL," +
                 "Anh TEXT NOT NULL)";
         db.execSQL(createTableDichvu);
 
@@ -49,29 +49,42 @@ public class Sqldatabase extends SQLiteOpenHelper {
                 "Id_tiec INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "Tentiec INTEGER NOT NULL," +
                 "Tenkhonggian INTEGER NOT NULL," +
+                "DanhGia TEXT NOT NULL," +
                 "Monan INTEGER NOT NULL)";
         db.execSQL(createTableTiec);
 
-        createTableMonan = "INSERT INTO  Monan   VALUES ( 1 ,'Nguyễn Phi Long','0000000113' ,'0000000113')";
-        db.execSQL(createTableMonan);
-        createTableMonan = "INSERT INTO  Monan   VALUES ( 2 ,'Chu van huy','0000000114' ,'0000000114')";
-        db.execSQL(createTableMonan);
-        createTableMonan = "INSERT INTO  Monan   VALUES ( 3 ,'Nguyễn Hoang Anh','0000000115' ,'0000000114')";
-        db.execSQL(createTableMonan);
+        String createThietlap = "create table Thietlap(" +
+                "Id_thietlap INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "Title INTEGER NOT NULL," +
+                "Review TEXT NOT NULL)";
+        db.execSQL(createThietlap);
+
+        String createLuutamthoi = "create table Luutamthoi(" +
+                "Id_luutamthoi INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "Title INTEGER NOT NULL," +
+                "Review TEXT NOT NULL)";
+        db.execSQL(createLuutamthoi);
 
 
-        createTableKhonggiantiec = "INSERT INTO  Khonggiantiec   VALUES ( 1 ,'Nguyễn Phi Long','image 1')";
+        createTableMonan = "INSERT INTO  Monan   VALUES ( 1 ,'Nguyễn Phi Long','anh1' ,'Tốt')";
+        db.execSQL(createTableMonan);
+        createTableMonan = "INSERT INTO  Monan   VALUES ( 2 ,'Chu van huy','anh2' ,'Khá')";
+        db.execSQL(createTableMonan);
+        createTableMonan = "INSERT INTO  Monan   VALUES ( 3 ,'Nguyễn Hoang Anh','anh3','Trung bình')";
+        db.execSQL(createTableMonan);
+
+        createTableKhonggiantiec = "INSERT INTO  Khonggiantiec   VALUES ( 1 ,'Nguyễn Phi Long','aaa','image 1')";
         db.execSQL(createTableKhonggiantiec);
-        createTableKhonggiantiec = "INSERT INTO  Khonggiantiec   VALUES ( 2 ,'Chu van huy','image 2')";
+        createTableKhonggiantiec = "INSERT INTO  Khonggiantiec   VALUES ( 2 ,'Chu van huy','bbbb','image 2')";
         db.execSQL(createTableKhonggiantiec);
-        createTableKhonggiantiec = "INSERT INTO  Khonggiantiec   VALUES ( 3 ,'Nguyễn Hoang Anh','image 3')";
+        createTableKhonggiantiec = "INSERT INTO  Khonggiantiec   VALUES ( 3 ,'Nguyễn Hoang Anh','cccc','image 3')";
         db.execSQL(createTableKhonggiantiec);
 
-        createTableDichvu = "INSERT INTO  Dichvu  VALUES ( 1 ,'Dam cuoi','sảnh lớn' ,'Bánh')";
+        createTableDichvu = "INSERT INTO  Dichvu  VALUES ( 1 ,'Dam cuoi','sảnh lớn' ,'Tốt','Bánh')";
         db.execSQL(createTableDichvu);
-        createTableDichvu = "INSERT INTO  Dichvu   VALUES ( 2 ,'Le hoi','Hội trường' ,'Kẹo')";
+        createTableDichvu = "INSERT INTO  Dichvu   VALUES ( 2 ,'Le hoi','Hội trường' ,'Khá','Kẹo')";
         db.execSQL(createTableDichvu);
-        createTableDichvu = "INSERT INTO  Dichvu   VALUES ( 3 ,'tot nghiep','Trường học' ,'Nước')";
+        createTableDichvu = "INSERT INTO  Dichvu   VALUES ( 3 ,'tot nghiep','Trường học','Khá' ,'Nước')";
         db.execSQL(createTableDichvu);
     }
 
